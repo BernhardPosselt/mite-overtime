8a6093b568feb208b8475c6b490e9b530b496f0d
Bernhard Posselt <dev@bernhard-posselt.com>
2015-10-10 17:22:14 +0200

    use locals url

--- routes/page.js
+++ routes/page.js
@@ -1,10 +1,12 @@
 'use strict';
 
+let locals = require('../local');
+
 module.exports = (app) => {
 
     app.get('/', (request, response) => {
         response.render('index', {
-            base: request.url
+            base: locals.baseUrl
         });
     });
 
