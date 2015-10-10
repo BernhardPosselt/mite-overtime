643fac78ac22251f8ea7e190ea397e91ce128227
Bernhard Posselt <dev@bernhard-posselt.com>
2015-10-10 04:44:57 +0200

    set api key in localstorage

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,12 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
     var form = document.getElementById('mite-form');
     var api = document.getElementById('form-api');
     var org= document.getElementById('form-org');
-    var result = document.getElementById('result');
-    var since = document.getElementById('form-since');
+    var out = document.getElementById('out');
     var perWeek = document.getElementById('form-perweek');
+
+    api.value = localStorage.getItem('apiKey');
 
     form.addEventListener('submit', function (event) {
         event.preventDefault();
+
+        // save api key in localstorage
+        localStorage.setItem('apiKey', api.value);
 
         var url = '/time-entries.json?' + $.param({
                 apiKey: api.value,
@@ -16,8 +20,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
         fetch(url).then(function (response) {
             return response.json();
-        }).then((result) => {
-           console.log(result);
+        }).then(function (result) {
+            var days = result.map(function (entry) {
+               return new Date(entry.time_entry_group.day);
+            }).sort(function (a, b) {
+                return a - b;
+            });
+
+            var minutesTotal = result.map(function (entry) {
+                return entry.time_entry_group.minutes;
+            }).reduce(function (previous, current) {
+                return previous + current;
+            });
+
+            var workedHours = Math.floor(minutesTotal/60);
+            var workedMintues = minutesTotal % 60;
+
+            out.innerHTML = "Worked " + workedHours + " hours and " + workedMintues + " minutes";
+        }).catch(function (error) {
+            console.log(error);
         });
 
         return false;
